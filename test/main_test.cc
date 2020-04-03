8bfdcacc2ea926dc096a1f72907543e41a94a960
Philip Salire <philip.s.salire@gmail.com>
2020-04-02 22:58:37 -0700

    tests

--- test/main_test.cc
+++ test/main_test.cc
@@ -92,61 +92,61 @@ int main(void) {
     assert(args.hash_to_crack == "fb8e20fc2e4c3f248c60c39bd652f3c1347298bb977b8b4d5903b85055620603");
     
     /**************************** Test string generator function ****************************/
-    std::cout << "hash_all_strings_up_to_len():";
-    std::cout << "  Search space: \"abcdefg\"\n";
-    std::cout << "  Max length: 7\n";
-    std::cout << "  Finding valid strings...\n";
+    std::cout << "[TEST] hash_all_strings_up_to_len():";
+    std::cout << "[TEST]   Search space: \"abcdefg\"\n";
+    std::cout << "[TEST]   Max length: 7\n";
+    std::cout << "[TEST]   Finding valid strings...\n";
     std::string ss = "abcdefg";
     for (std::string s : {"a", "g", "fe", "bbg", "", "ggaf", "ggggggg", "bacedd", ss.c_str()}) {
-        std::cout << "  -Finding \"" << s << "\"...\n";
+        std::cout << "[TEST]   -Finding \"" << s << "\"...\n";
         assert(hash_all_strings_up_to_len(ss, ss.size(), "", s));
     }
-    std::cout << "  Finding invalid strings...\n";
+    std::cout << "[TEST]   Finding invalid strings...\n";
     for (std::string s : {"x", "ggz", "aaaaaaaa", "!!!!!"}) {
-        std::cout << "  -Finding \"" << s << "\"...\n";
+        std::cout << "[TEST]   -Finding \"" << s << "\"...\n";
         assert(!hash_all_strings_up_to_len(ss, ss.size(), "", s));
     }
-    std::cout << " Prepend val \"g\"\n";
-    std::cout << " Finding valid strings...\n";
+    std::cout << "[TEST]  Prepend val \"g\"\n";
+    std::cout << "[TEST]  Finding valid strings...\n";
     for (std::string s : {"g", "ggb", "gaaaaaa", "gbcefg"}) {
-        std::cout << "  -Finding \"" << s << "\"...\n";
+        std::cout << "[TEST]   -Finding \"" << s << "\"...\n";
         assert(hash_all_strings_up_to_len(ss, ss.size(), "g", s));
     }
-    std::cout << " Finding invalid strings...\n";
+    std::cout << "[TEST]  Finding invalid strings...\n";
     for (std::string s : {"", "xgb", "a!", "gaaaaa@", "abc", "gago", "gggggggg"}) {
-        std::cout << "  -Finding \"" << s << "\"...\n";
+        std::cout << "[TEST]   -Finding \"" << s << "\"...\n";
         assert(!hash_all_strings_up_to_len(ss, ss.size(), "g", s));
     }
-    std::cout << " Prepend val \"ab\"\n";
-    std::cout << " Finding valid strings...\n";
+    std::cout << "[TEST]  Prepend val \"ab\"\n";
+    std::cout << "[TEST]  Finding valid strings...\n";
     for (std::string s : {"ab", "abc", "abgge", "abbbbbe"}) {
-        std::cout << "  -Finding \"" << s << "\"...\n";
+        std::cout << "[TEST]   -Finding \"" << s << "\"...\n";
         assert(hash_all_strings_up_to_len(ss, ss.size(), "ab", s));
     }
-    std::cout << " Finding invalid strings...\n";
+    std::cout << "[TEST]  Finding invalid strings...\n";
     for (std::string s : {"", "abbbbbee", "abbbbe!", "abbx", "x", "ab!", "abababababab"}) {
-        std::cout << "  -Finding \"" << s << "\"...\n";
+        std::cout << "[TEST]   -Finding \"" << s << "\"...\n";
         assert(!hash_all_strings_up_to_len(ss, ss.size(), "ab", s));
     }
     
     /**************************** Test verify hash functions ****************************/
     HashCrack<CryptoPP::SHA256> test_verify_hash(args.hash_to_crack, "", "", 0, 0, false);
-    std::cout << "check_hash_match():\n";
-    std::cout << "  -Verify hash against incorrect strings...\n";
+    std::cout << "[TEST] check_hash_match():\n";
+    std::cout << "[TEST]   -Verify hash against incorrect strings...\n";
     for (std::string s : {"Hello world!", "Goodbye world!", "abc", "aa", "ba", "a", ""}) {
-        std::cout << "  -Verifying \"" << s << "\"\n";
+        std::cout << "[TEST]   -Verifying \"" << s << "\"\n";
         assert(!test_verify_hash.check_hash_match(s));
     }
-    std::cout << "  -Verify hash against correct string...\n";
+    std::cout << "[TEST]   -Verify hash against correct string...\n";
     assert(test_verify_hash.check_hash_match("ab"));
     
     HashCrack<CryptoPP::SHA1> test_verify_hash_1("da23614e02469a0d7c7bd1bdab5c9c474b1904dc", "", "", 0, 0, false);
-    std::cout << "  -Verify hash against incorrect strings...\n";
+    std::cout << "[TEST]   -Verify hash against incorrect strings...\n";
     for (std::string s : {"Hello world!", "Goodbye world!", "abc", "aa", "ba", "a", ""}) {
-        std::cout << "  -Verifying \"" << s << "\"\n";
+        std::cout << "[TEST]   -Verifying \"" << s << "\"\n";
         assert(!test_verify_hash_1.check_hash_match(s));
     }
-    std::cout << "  -Verify hash against correct string...\n";
+    std::cout << "[TEST]   -Verify hash against correct string...\n";
     assert(test_verify_hash_1.check_hash_match("ab"));
 
     /**************************** Create server instance ****************************/
@@ -156,10 +156,9 @@ int main(void) {
         args.hash_algo,
         2, 8080, 0, false
     );
-    
-    /**************************** Test server client connection ****************************/
-    /* Tuple: (#clients, #threads per client, expected divisions of prefixes, search space,
-                hash of string, input string, hash algo) */
+    /**************************** Test distributed cracking ****************************/
+    /* Tuple: (#clients, #threads for each client, search space, expected divisions of prefixes,
+                hash of string, input string, hash algo, max_string_len, use_fixed_string_len) */
     for (auto tuple : std::vector<std::tuple<int, std::vector<int>, std::string, std::vector<std::string>,
                                              std::string, std::string, std::string, int, bool>>{
         std::make_tuple(
@@ -175,7 +174,8 @@ int main(void) {
         std::make_tuple(
             3, std::vector<int>{2,1,1}, args.search_space,
             std::vector<std::string>{"abcdefghijklmnopqrstu", "vwxyzABCDEFGHIJKLMNOP", "QRSTUVWXYZ0123456789!@"},
-            "016c756f0e615ef70ca05eb499a74d31c2ddec7244760d59d792583c0410b36d", "boop", "SHA256", 0, false
+            "a6a92fce880317fae5cf6bfc47605d39d7347c797c43d708909e73604919b67e0c79124143a3480cf6fefb6c282dbd67",
+            "boop", "SHA384", 0, false
         ),
         std::make_tuple(
             4, std::vector<int>{1,1,1,1}, args.search_space,
@@ -188,33 +188,45 @@ int main(void) {
             "a9993e364706816aba3e25717850c26c9cd0d89d", "abc", "SHA1", 0, false
         ),
         std::make_tuple(
+            1, std::vector<int>{3}, args.search_space,
+            std::vector<std::string>{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"},
+            "ad6afc3e4a3505962a11e8bd593a57eeb36663f884607f9a7af0d27c21acc42b14b437ee604a4241231038121096ee8b",
+            "dcba", "SHA3_384", 0, false
+        ),
+        std::make_tuple(
             3, std::vector<int>{1,2,1}, args.search_space,
             std::vector<std::string>{"abcdefghijklmnopqrstu", "vwxyzABCDEFGHIJKLMNOP", "QRSTUVWXYZ0123456789!@"},
-            "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0", "Hello", "SHA1", 0, false
+            "3615f80c9d293ed7402687f94b22d58e529b8cc7916f8fac7fddf7fbd5af4cf777d3d795a7a00a16bf7e7f3fb9561ee9baae480da9fe7a18769e71886b03f315",
+            "Hello", "SHA512", 5, true
         ),
         std::make_tuple(
             2, std::vector<int>{1,3}, args.search_space,
             std::vector<std::string>{"abcdefghijklmnopqrstuvwxyzABCDEF", "GHIJKLMNOPQRSTUVWXYZ0123456789!@"},
-            "8f7beab00c9b839a44173edb77bdf6f9062b1e7023b06c22013ae2b983945292", "XaaaB", "SHA256", 0, false
+            "8f7beab00c9b839a44173edb77bdf6f9062b1e7023b06c22013ae2b983945292", "XaaaB", "SHA256", 5, true
         ),
         std::make_tuple(
+            5, std::vector<int>{1,1,1,1,1}, args.search_space,
+            std::vector<std::string>{"abcdefghijkl", "mnopqrstuvwx8", "yzABCDEFGHIJ9", "KLMNOPQRSTUV!", "WXYZ01234567@"},
+            "b39ed49e3f914850e26a080b26228be16c06aacb3a839ecca1edd6748b58efb2", "!@!@", "SHA3_256", 4, true
+        )
+        /* std::make_tuple(
             4, std::vector<int>{1,1,1,1}, args.search_space,
             std::vector<std::string>{"abcdefghijklmnop", "qrstuvwxyzABCDEF", "GHIJKLMNOPQRSTUV", "WXYZ0123456789!@"},
-            "38448a5ac7723caefeba44b9e96bf856d16e8a8b04a588b32b1e6e608e170e91", "worxX", "SHA256", 0, false
-        )
+            "38448a5ac7723caefeba44b9e96bf856d16e8a8b04a588b32b1e6e608e170e91", "worxX", "SHA256", 5, true
+        ) */
     }) {
-        int num_clients = std::get<0>(tuple);
         /* Give meaning to tuple values */
-        std::vector<int> client_thread_counts = std::get<1>(tuple);
-        assert((unsigned int) num_clients == client_thread_counts.size());
-        std::string search_space = std::get<2>(tuple);
+        int num_clients = std::get<0>(tuple);
+        std::vector<int> client_thread_counts      = std::get<1>(tuple);
+        std::string search_space                   = std::get<2>(tuple);
         std::vector<std::string> expected_prefixes = std::get<3>(tuple);
+        std::string hash_to_crack                  = std::get<4>(tuple);
+        std::string correct_crack                  = std::get<5>(tuple);
+        std::string hash_algo                      = std::get<6>(tuple);
+        unsigned int max_string_len                = std::get<7>(tuple);
+        bool use_fixed_string_len                  = std::get<8>(tuple);
+        assert((unsigned int) num_clients == client_thread_counts.size());
         assert((unsigned int) num_clients == expected_prefixes.size());
-        std::string hash_to_crack = std::get<4>(tuple);
-        std::string correct_crack = std::get<5>(tuple);
-        std::string hash_algo = std::get<6>(tuple);
-        unsigned int max_string_len = std::get<7>(tuple);
-        bool use_fixed_string_len = std::get<8>(tuple);
         
         server_crack.set_total_clients(num_clients);
         server_crack.set_hash_to_crack(hash_to_crack);
@@ -223,54 +235,55 @@ int main(void) {
         server_crack.set_use_fixed_string_length(use_fixed_string_len);
         server_crack.set_hash_algo(hash_algo);
         
-        std::cout << "\nTesting:\n";
-        std::cout << "-Total Clients: " << num_clients << "\n";
-        std::cout << "-Hash to Crack: " << hash_to_crack << "\n";
-        std::cout << "-Hash Algo: " << hash_algo << "\n";
-        std::cout << "-Search Space: " << search_space << "\n";
-        std::cout << "-Max String Len: " << max_string_len << "\n";
-        std::cout << "-Use Fixed String Len: " << use_fixed_string_len << "\n";
-            
+        std::cout << "\n[TEST] Testing:\n";
+        std::cout << "[TEST] -Total Clients: " << num_clients << "\n";
+        std::cout << "[TEST] -Hash to Crack: " << hash_to_crack << "\n";
+        std::cout << "[TEST] -Hash Algo: " << hash_algo << "\n";
+        std::cout << "[TEST] -Search Space: " << search_space << "\n";
+        std::cout << "[TEST] -Max String Len: " << max_string_len << "\n";
+        std::cout << "[TEST] -Use Fixed String Len: " << (use_fixed_string_len ? "True" : "False") << "\n";
+        
+        /**************************** Test server client connection ****************************/
         /* Server accept clients */
         std::thread thread_accept = std::thread(&HashCrackServer::accept_clients, std::ref(server_crack));
         
         /* Create clients to connect to server */
         std::vector<HashCrackClient *> client_comms;
         std::vector<std::thread> client_comms_threads;
+        assert(client_comms.size() == 0);
+        assert(client_comms_threads.size() == 0);
         for (int i = 0; i < num_clients; i++) {
             client_comms.push_back(new HashCrackClient(client_thread_counts[i]));
             client_comms_threads.push_back(std::thread(&HashCrackClient::connect_to_server, std::ref(*client_comms[i]), "127.0.0.1", 8080));
         }
         
-        std::cout << "Joining at line " << __LINE__ << "...\n";
+        std::cout << "[TEST] Joining at line " << __LINE__ << "...\n";
         thread_accept.join();
-        puts("start()...");
+        puts("[TEST] start()...");
         server_crack.start();
         for (int i = 0; i < num_clients; i++) {
-            std::cout << "Joining at line " << __LINE__ << "...\n";
+            std::cout << "[TEST] Joining at line " << __LINE__ << "...\n";
             client_comms_threads[i].join();
             assert(client_comms[i]->get_is_connected());
         }
-        std::cout << "done\n";
+        // std::cout << "done\n";
 
         /**************************** Test recvd settings correct ****************************/
         std::vector<Settings> settings;
+        assert(settings.size() == 0);
         for (int i = 0; i < num_clients; i++) {
             settings.push_back(client_comms[i]->get_settings());
-        }
-        int settings_i = 0;
-        for (Settings s : settings) {
-            std::cout << "Verifiying settings [" << settings_i++ << "]...\n";
+            std::cout << "[TEST] Verifiying settings [" << i << "]...\n";
             // std::cout << s.hash_algo << "==" << hash_algo << "\n";
-            assert(s.hash_algo == hash_algo);
+            assert(settings[i].hash_algo == hash_algo);
             // std::cout << s.search_space << "==" << search_space << "\n";
-            assert(s.search_space == search_space);
+            assert(settings[i].search_space == search_space);
             // std::cout << s.hash_to_crack << "==" << hash_to_crack << "\n";
-            assert(s.hash_to_crack == hash_to_crack);
+            assert(settings[i].hash_to_crack == hash_to_crack);
             // std::cout << s.use_fixed_string_len << "==" << use_fixed_string_len << "\n";
-            assert(s.use_fixed_string_len == use_fixed_string_len);
+            assert(settings[i].use_fixed_string_len == use_fixed_string_len);
             // std::cout << s.max_string_len << "==" << max_string_len << "\n";
-            assert(s.max_string_len == max_string_len);
+            assert(settings[i].max_string_len == max_string_len);
         }
         for (unsigned int i = 0; i < settings.size(); i++) {
             bool prefix_match = false;
@@ -287,11 +300,16 @@ int main(void) {
         assert(expected_prefixes.size() == 0);
         
         /* Function to do distributed cracking */
-        auto do_crack = [&server_crack, &client_comms, correct_crack, num_clients](auto client_cracks_vec) {
+        auto do_crack = [&server_crack, &client_comms, hash_algo, correct_crack,
+                         num_clients, client_thread_counts](auto client_cracks_vec) {
             /**************************** Test distributed crack ****************************/
             std::chrono::steady_clock::time_point start, end;
-            printf("Cracking hashed string of \"%s\" with %d clients...\n", correct_crack.c_str(), num_clients);
+            printf("[TEST] Cracking hashed %s string of \"%s\" with %d clients...\n", hash_algo.c_str(), correct_crack.c_str(), num_clients);
+            for (int i = 0; i < num_clients; i++) {
+                printf("[TEST] Client #%d using %d threads\n", i, client_thread_counts[i]);
+            }
             std::vector<std::thread> crack_threads;
+            assert(crack_threads.size() == 0);
             start = std::chrono::steady_clock::now();
             for (int i = 0; i < num_clients; i++) {
                 crack_threads.push_back(std::thread([&, i]() {
@@ -300,14 +318,14 @@ int main(void) {
                 }));
             }
             for (std::thread &t : crack_threads) {
-                std::cout << "Joining at line " << __LINE__ << "...\n";
+                std::cout << "[TEST] Joining at line " << __LINE__ << "...\n";
                 t.join();
             }
 
-            puts("wait_for_clients()...");
+            puts("[TEST] wait_for_clients()...");
             server_crack.wait_for_clients();
             end = std::chrono::steady_clock::now();
-            std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "s\n";
+            std::cout << "[TEST] Time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "s\n";
 
             /**************************** Test results correct ****************************/
             bool is_cracked = false;
@@ -336,6 +354,7 @@ int main(void) {
         /**************************** Create HashCracks from settings ****************************/
         if (hash_algo == "SHA256") {
             std::vector<HashCrack<CryptoPP::SHA256> *> client_cracks;
+            assert(client_cracks.size() == 0);
             for (int i = 0; i < num_clients; i++) {
                 client_cracks.push_back(new HashCrack<CryptoPP::SHA256>(settings[i], client_thread_counts[i]));
             }
@@ -343,8 +362,57 @@ int main(void) {
         }
         else if (hash_algo == "SHA1") {
             std::vector<HashCrack<CryptoPP::SHA1> *> client_cracks;
+            assert(client_cracks.size() == 0);
             for (int i = 0; i < num_clients; i++) {
                 client_cracks.push_back(new HashCrack<CryptoPP::SHA1>(settings[i], client_thread_counts[i]));
+            }
+            do_crack(client_cracks);
+        }
+        else if (hash_algo == "SHA384") {
+            std::vector<HashCrack<CryptoPP::SHA384> *> client_cracks;
+            assert(client_cracks.size() == 0);
+            for (int i = 0; i < num_clients; i++) {
+                client_cracks.push_back(new HashCrack<CryptoPP::SHA384>(settings[i], client_thread_counts[i]));
+            }
+            do_crack(client_cracks);
+        }
+        else if (hash_algo == "SHA512") {
+            std::vector<HashCrack<CryptoPP::SHA512> *> client_cracks;
+            assert(client_cracks.size() == 0);
+            for (int i = 0; i < num_clients; i++) {
+                client_cracks.push_back(new HashCrack<CryptoPP::SHA512>(settings[i], client_thread_counts[i]));
+            }
+            do_crack(client_cracks);
+        }
+        else if (hash_algo == "SHA3_224") {
+            std::vector<HashCrack<CryptoPP::SHA3_224> *> client_cracks;
+            assert(client_cracks.size() == 0);
+            for (int i = 0; i < num_clients; i++) {
+                client_cracks.push_back(new HashCrack<CryptoPP::SHA3_224>(settings[i], client_thread_counts[i]));
+            }
+            do_crack(client_cracks);
+        }
+        else if (hash_algo == "SHA3_256") {
+            std::vector<HashCrack<CryptoPP::SHA3_256> *> client_cracks;
+            assert(client_cracks.size() == 0);
+            for (int i = 0; i < num_clients; i++) {
+                client_cracks.push_back(new HashCrack<CryptoPP::SHA3_256>(settings[i], client_thread_counts[i]));
+            }
+            do_crack(client_cracks);
+        }
+        else if (hash_algo == "SHA3_384") {
+            std::vector<HashCrack<CryptoPP::SHA3_384> *> client_cracks;
+            assert(client_cracks.size() == 0);
+            for (int i = 0; i < num_clients; i++) {
+                client_cracks.push_back(new HashCrack<CryptoPP::SHA3_384>(settings[i], client_thread_counts[i]));
+            }
+            do_crack(client_cracks);
+        }
+        else if (hash_algo == "SHA3_512") {
+            std::vector<HashCrack<CryptoPP::SHA3_512> *> client_cracks;
+            assert(client_cracks.size() == 0);
+            for (int i = 0; i < num_clients; i++) {
+                client_cracks.push_back(new HashCrack<CryptoPP::SHA3_512>(settings[i], client_thread_counts[i]));
             }
             do_crack(client_cracks);
         }
